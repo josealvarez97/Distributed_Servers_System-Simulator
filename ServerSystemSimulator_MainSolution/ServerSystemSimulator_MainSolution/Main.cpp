1304d1a70a8a9743dc73e3add313d6bc5d1bde8c
oswilehi <oswilehi@gmail.com>
2016-10-17 10:50:04 -0600

    Added the delet for some nodes

--- ServerSystemSimulator_MainSolution/ServerSystemSimulator_MainSolution/Main.cpp
+++ ServerSystemSimulator_MainSolution/ServerSystemSimulator_MainSolution/Main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main()
 {
 	cout << "Hola mundo" << endl;
+	string palabra = "";
+	for (size_t i = 0; i < 5; i++)
+	{
+		palabra = palabra + "*";
+	}
+
+	cout << palabra.c_str();
 
 	cin.get();
 	return 0;
