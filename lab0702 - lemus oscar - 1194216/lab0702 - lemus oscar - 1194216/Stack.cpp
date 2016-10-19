fc4b50af98f9588daa992d9e9ba8d4b9459602e4
jose_alvarez_cabrera <cachorritoalfa2013@gmail.com>
2016-10-18 19:38:26 -0600

    Added Stack Class

---
+++ lab0702 - lemus oscar - 1194216/lab0702 - lemus oscar - 1194216/Stack.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include "Stack.h"
+
+
+CStack::CStack()
+{
+}
+
+
+CStack::~CStack()
+{
+}
+
+int CStack::Size()
+{
+	return this->counter;
+}
+
+void CStack::Push(int number)
+{
+	list.Add(number);
+	counter++;
+}
+
+int CStack::Pop()
+{
+	if (list.Size() != 0)
+	{
+		int element = list.LastElement();
+		list.RemoveTail();
+		counter--;
+		return element;
+	}
+
+	return -1;
+
+}
+
+int CStack::Top()
+{
+	if (list.Size() != 0)
+		return list.LastElement();
+	return -1;
+}
